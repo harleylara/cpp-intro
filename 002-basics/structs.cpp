c46ba582a15ed526248b1b4a276c7aafc8b6a1cd
harleylara <harley.lara@outlook.com>
2024-03-05 10:35:49 +0100

    add struct demo

---
+++ 002-basics/structs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+
+    struct linear_vel {
+        double x;
+        double y;
+        double z;
+    };
+
+    linear_vel robot_vel;
+
+    robot_vel.x = 1;
+    robot_vel.y = 0;
+    robot_vel.z = 0;
+
+    std::cout << robot_vel.x << std::endl;
+    std::cout << robot_vel.y << std::endl;
+    std::cout << robot_vel.z << std::endl;
+
+    return  0;
+}
