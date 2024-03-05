f7c6c79ef3304b05ffac92158d32047d2c993fc0
harleylara <harley.lara@outlook.com>
2024-03-05 11:12:39 +0100

    add print function

--- 002-basics/functions.cpp
+++ 002-basics/functions.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <string> // to use string class
+
+void print(std::string message)
+{
+    std::cout << message << std::endl;
+}
 
 double get_acc(double delta_vel, double delta_t)
 {
     return delta_vel/delta_t;
 }
 
+
 int main(int argc, char* argv[])
 {
+    print("Python is cool");
+
     std::cout << get_acc(0.3, 1.2) << std::endl;
 
     return 0;
