f33d88870cde6db634d1f3f63a6a92546e9c3604
harleylara <harley.lara@outlook.com>
2024-03-07 15:50:30 +0100

    better format

--- 002-basics/var.cpp
+++ 002-basics/var.cpp
@@ -2,17 +2,31 @@
 
 int main(int argc, char* argv[])
 {
-    int foo;        // this is declaration
-    int bar = 1;    // this is initialization
+    // 1. Direct-initialization
+    // stack memory allocation
+    int foo;
+    // garbage value
+    std::cout << "The content of foo is: " << foo << std::endl;
 
-    // Three ways to initialize variables
-    int a=5;    // c-like initialization
-    int b(3);   // constructor initialization
-    int c{2};   // uniform initialization
+    // 2. Copy-initialization
+    // c-like initialization
+    int bar = 1;
+    std::cout << "The content of bar is: " << bar << std::endl;
+    int var = (((29))); // just to make the point
+    // in this context the parenthesis is an "expression" and when the expression is evaluated the result is still 29.
+    std::cout << "The content of var is: " << var << std::endl;
 
-    auto result = a + b + c; // type deduction
+    // 3. Aggregate initialization. 
+    // List-initialization syntax. (since C++11)
+    int foobar = {2};
+    std::cout << "The content of foobar is: " << foobar << std::endl;
 
-    std::cout << result << std::endl;
+    // 4. Direct-initialization
+    int b(7);
+    std::cout << "The content of b is: " << b << std::endl;
+
+    int c(); // this is a function declaration NOT a variable declaration
+    int d{2}; // Direct-initialization using List-initialization syntax (since C++11)
 
     return 0;
 }
