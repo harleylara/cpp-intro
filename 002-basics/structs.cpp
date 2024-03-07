7f692f108f8bdfae46f6976fb4a04f4f7b65ad2b
harleylara <harley.lara@outlook.com>
2024-03-07 23:25:37 +0100

    move struct to global definition

--- 002-basics/structs.cpp
+++ 002-basics/structs.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 
+// this can reprents any 3 dimentional
+// vector. For exmaple linear velocity in x, y and z
+// Struct is a user definend data container.
+struct vec3_t {
+    float x, y, z;
+};
+
 int main(int argc, char* argv[])
 {
+    vec3_t vel;
 
-    struct linear_vel_t {
-        double x, y, z;
+    vel.x = 1;
+    vel.y = 0;
+    vel.z = 0;
+
+    std::cout << vel.x << std::endl;
+    std::cout << vel.y << std::endl;
+    std::cout << vel.z << std::endl;
+
+    struct twist {
+        vec3_t linear, angular;
     };
 
-    linear_vel_t robot_vel;
+    twist robot_twist;
 
-    robot_vel.x = 1;
-    robot_vel.y = 0;
-    robot_vel.z = 0;
+    robot_twist.linear.x = 1.2;
+    robot_twist.linear.y = 3.5;
+    robot_twist.linear.z = 0;
+    robot_twist.angular.x = 0.6;
+    robot_twist.angular.y = 1.1;
+    robot_twist.angular.z = 0;
 
-    std::cout << robot_vel.x << std::endl;
-    std::cout << robot_vel.y << std::endl;
-    std::cout << robot_vel.z << std::endl;
+    std::cout << "Robot twist information" << std::endl;
+    std::cout << robot_twist.linear.x << std::endl;
+    std::cout << robot_twist.linear.y << std::endl;
+    std::cout << robot_twist.linear.z << std::endl;
+    std::cout << robot_twist.angular.x << std::endl;
+    std::cout << robot_twist.angular.y << std::endl;
+    std::cout << robot_twist.angular.z << std::endl;
+
 
     return  0;
 }
