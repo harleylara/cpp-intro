3e9bb2814a14e2cfa99dad4d86783ffdbb16e3b4
harleylara <harley.lara@outlook.com>
2024-03-07 19:22:42 +0100

    add example const

--- 002-basics/functions.cpp
+++ 002-basics/functions.cpp
@@ -1,22 +1,48 @@
 #include <iostream>
 #include <string> // to use string class
 
-void print(std::string message)
+void print(const std::string message)
 {
     std::cout << message << std::endl;
 }
 
-double get_acc(double delta_vel, double delta_t)
+// passing arguments "by value"
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+// passing arguments "by reference"
+double get_acc(double &delta_vel, double &delta_t)
 {
     return delta_vel/delta_t;
 }
 
+// return a const reference to the max value
+const int& getMax(const int& a, const int& b) {
+    return (a > b) ? a : b;
+}
 
 int main(int argc, char* argv[])
 {
     print("Python is cool");
 
-    std::cout << get_acc(0.3, 1.2) << std::endl;
+    int a = 6;
+    int b = 2;
+    std::cout << "sum return " << sum(a, b) << std::endl;
+
+    double delta_vel = 0.3;
+    double delta_t = 1.2;
+
+    std::cout << get_acc(delta_vel, delta_t) << std::endl;
+
+    int x = 5, y = 10;
+    const int& maxVal = getMax(x, y);
+    std::cout << "Max value: " << maxVal << std::endl;
+    // if you change "y" (in the case the max value)
+    y = 30;
+    // maxVal also change
+    std::cout << "Max value: " << maxVal << std::endl;
 
     return 0;
 }
