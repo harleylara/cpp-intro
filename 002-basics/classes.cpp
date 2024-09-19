4aa695f83c24872dab8c8c731ff547b2d4b6a22d
harleylara <harley.lara@outlook.com>
2024-09-19 03:44:12 +0200

    improve sequence of class section

--- 002-basics/classes.cpp
+++ 002-basics/classes.cpp
@@ -6,17 +6,14 @@ struct vec3_t {
     float x, y, z;
 };
 
-class RobotBase
+class SimpleRobot
 {
     std::string name_;
     uint8_t battery_; // battery level in percentage
 public:
-    vec3_t position{};
+    // vec3_t position;
+    vec3_t position{}; // possible fix
 
-    RobotBase() : name_("robot_name"), battery_(0) {}
-
-    RobotBase(std::string name): name_(name), battery_(0) {}
-    RobotBase(std::string name, uint8_t battery): name_(name), battery_(battery) {}
 
     int getBattery() {
         return battery_;
@@ -24,35 +21,97 @@ public:
 
     std::string getName() {
         return name_;
+    }
 
+    void setName(std::string name) {
+        name_ = name;
     }
 };
 
-class Drone: public RobotBase
+class BetterRobot
 {
+    std::string name_;
+    uint8_t battery_;
 public:
-    float barometer = 0.0; // extending class with more members
+    vec3_t position{};
+
+    BetterRobot() : name_("robot_name"), battery_(0) {}
+
+    // BetterRobot(std::string name): name_(name) {}
+    BetterRobot(std::string name): name_(name), battery_(0) {}
+
+    BetterRobot(std::string name, uint8_t battery): name_(name), battery_(battery) {}
+
+    int getBattery() {
+        return battery_;
+    }
+
+    std::string getName() {
+        return name_;
+    }
+
+    void setName(std::string name) {
+        name_ = name;
+    }
+
+    // void info() {
+    //     std::cout << "\nName: " << name_ << std::endl;
+    //     std::cout << "Battery: " << unsigned(battery_) << std::endl;
+    //     std::cout << std::endl;
+    // }
+    void info();
+
 };
+
+// using the scope operator for extending the class
+// with the implementation of a member function
+void BetterRobot::info() {
+    std::cout << "\nName: " << name_ << std::endl;
+    std::cout << "Battery: " << unsigned(battery_) << std::endl;
+    std::cout << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
 
-    RobotBase robile;
-    std::cout << robile.getName() <<  "'s position in x " <<  robile.position.x << std::endl;
-    std::cout << robile.getName() <<  "'s battery: " <<  robile.getBattery() << std::endl;
+    SimpleRobot robot;
 
-    // Since C++ 11 you can use agregate initialization
-    // https://en.cppreference.com/w/cpp/language/aggregate_initialization
-    // set data members to zero
-    RobotBase robot {"freddy", 100};
-    std::cout << robot.getName() << "'s position in x " <<  robot.position.x << std::endl;
-    std::cout << robot.getName() << "'s battery: " << robot.getBattery() << std::endl;
+    // ## Members initialization ##
 
-    Drone drone;
-    // the inheritance of the `RobotBase` class
-    // allows us to inherit the position member in the `Drone` class
-    std::cout << drone.getName() <<  "'s position in x " << drone.position.x << std::endl;
-    std::cout << drone.getName() << "'s barometer value " << drone.barometer << std::endl;
+    // what is the initial value of the position on x?
+    // [FIX] initialize the position to zero
+    std::cout << "position in x: " << robot.position.x << std::endl;
+
+    // how to access a private member outside the class ?
+    // std::cout << "Name: " << robot.name_ << std::endl; // this is private
+    // [FIX] getter interface to read a private member
+    std::cout << "Name: " << robot.getName() << std::endl; // empty right?
+
+    // how to set a private member?
+    // robot.name_ =  "robile";
+    // robot.name =  "robile";
+    robot.setName("robile");
+    std::cout << "Name: " << robot.getName() << std::endl;
+
+    // can we initialize members with **default values**
+    // during object instantiation?
+    BetterRobot quadruped;
+    // [FIX] default initialization
+    quadruped.info();
+
+    // [FIX] what if we want to pass the name as argument during instantiation?
+    BetterRobot other_quadruped("nala");
+    std::cout << "Name: " << other_quadruped.getName() << ", Battery: " << other_quadruped.getBattery() << std::endl;
+    // [FIX] add a member function to print the information
+    other_quadruped.info();
+
+    // [FIX] what if I want to initialize name and battery ?
+    BetterRobot one_more_quadruped("nala", 100);
+    one_more_quadruped.info();
+
+    // using uniform initialization
+    BetterRobot its_the_last_one_i_promise = {"nala", 100};
+    its_the_last_one_i_promise.info();
 
     return 0;
 }
